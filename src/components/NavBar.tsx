3693a59f2361d610d64f162f4cc9927dd5c0979b
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-06-08 13:43:25 +0200

    Cart Component to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,28 @@
 import { NavLink } from "react-router-dom";
 import { AiOutlineShopping } from "react-icons/ai";
+
+import Cart from "./Cart";
+
+import { useStateContext } from "../../context/StateContext";
+
 const NavBar = () => {
+  const { showCart, setShowCart, totalQuantities }: any = useStateContext();
   return (
     <div className="navbar-container">
       <p className="logo">
         <NavLink to="/">Accueil</NavLink>
       </p>
-      <button type="button" className="cart-icon" onClick={() => {}}>
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => {
+          setShowCart(!showCart);
+        }}
+      >
         <AiOutlineShopping />
-        <span className="cart-item-qty">1 </span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+      {showCart && <Cart />}
     </div>
   );
 };

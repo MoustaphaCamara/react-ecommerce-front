8d3003f0d6304fa20e75a6ded38075f96f698024
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-06-08 11:59:14 +0200

    update global context with cart functionality

--- context/StateContext.tsx
+++ context/StateContext.tsx
@@ -1,14 +1,39 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { toast } from "react-hot-toast";
+import { ProductInt } from "../src/models/product";
 
-const Context = createContext();
+const Context = createContext(null!);
 
-export const StateContext = ({ children }) => {
+export const StateContext = ({ children }: React.FC | any) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState();
-  const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantities, settotalQuantities] = useState();
+  const [cartItems, setCartItems] = useState<any[]>([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qtt, setQtt] = useState(1);
+
+  const onAdd = (product: ProductInt, quantity: number) => {
+    const checkIfProductInCart = cartItems.find(
+      (item: any) => item._id === product._id
+    );
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+    if (checkIfProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qtt} ${product.name} added to the cart.`);
+  };
 
   const incQtt = () => {
     setQtt((prevQtt) => prevQtt + 1);
@@ -23,12 +48,14 @@ export const StateContext = ({ children }) => {
     <Context.Provider
       value={{
         showCart,
+        setShowCart,
         cartItems,
         totalPrice,
         totalQuantities,
         qtt,
         incQtt,
         decQtt,
+        onAdd,
       }}
     >
       {children}
@@ -36,4 +63,4 @@ export const StateContext = ({ children }) => {
   );
 };
 
-export default StateContext;
+export const useStateContext = () => useContext(Context);
